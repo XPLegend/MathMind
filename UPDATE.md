4e4a00c5403a7fd2022f51e76bc7479241273d81
XPLegend <110475641+XPLegend@users.noreply.github.com>
2023-11-18 17:12:52 +0000

    V0.5 RELEASE

--- UPDATE.md
+++ UPDATE.md
@@ -4,5 +4,9 @@
 -Added basic HTML and CSS
 
 17/11/2023
-
 Started working on the basic javascript code, on my way to version 0.5
+
+V0.5 RELEASE
+18/11/2023  
+Finished basic JS code and modified "settings bar" CSS to prettyfy it, little modifications to HTML to simplyfy the interface.
+
--- index.js
+++ index.js
@@ -27,58 +27,75 @@ let strNumber = 0;
 let ctrNumber = 0;
 
 
-//CREATE THE NUMBER BY PUSHING THE DIGITS IN IT
+//CREATE THE NUMBER BY PUSHING THE DIGITS IN IT; "THE ARRAY"
 let userResult = [];
 
-//KEYBOARD KEYS
+//KEYBOARD KEYS CODE
 document.getElementById("one").onclick = function add1() {
-    userResult.push(1);
+    userResult.push("1");
 }
 document.getElementById("two").onclick = function add2() {
-    userResult.push(2);
+    userResult.push("2");
 }
 document.getElementById("three").onclick = function add3() {
-    userResult.push(3);
+    userResult.push("3");
 }
 document.getElementById("four").onclick = function add4() {
-    userResult.push(4);
+    userResult.push("4");
 }
 document.getElementById("five").onclick = function add5() {
-    userResult.push(5);
+    userResult.push("5");
 }
 document.getElementById("six").onclick = function add6() {
-    userResult.push(6);
+    userResult.push("6");
 }
 document.getElementById("seven").onclick = function add7() {
-    userResult.push(7);
+    userResult.push("7");
 }
 document.getElementById("eight").onclick = function add8() {
-    userResult.push(8);
+    userResult.push("8");
 }
 document.getElementById("nine").onclick = function add9() {
-    userResult.push(9);
+    userResult.push("9");
 }
 document.getElementById("zero").onclick = function add0() {
-    userResult.push(0);
+    userResult.push("0");
 }
 
 //SEND FINAL RESULT
 document.getElementById("submit").onclick = function submit() {
+    //create procedural verifier
     let res = userResult.reduce((a, b) => a + b, 0)
 
+    //check result
+    if (res == firstNumber + secondNumber) {
+        scrNumber += 10;
+        strNumber += 1;
+        ctrNumber += 1;
+    } else {
+        strNumber = 0;
+        ctrNumber += 1;
+    }
+
+    document.getElementById("scr-number").innerHTML = scrNumber;
+    document.getElementById("str-number").innerHTML = strNumber;
+    document.getElementById("ctr-number").innerHTML = ctrNumber;
+
+    //empty "the array" 
+    userResult = [];
+
     //DEBUG
-    console.log(res);
+    console.log(`User answer: ${res}`);
 }
 
 //DIFFICULTIES
 document.getElementById("gen").onclick = function difficulty1() {
     firstNumber = Math.floor(Math.random() * 9);
     secondNumber = Math.floor(Math.random() * 9);
-    let equation = `${firstNumber} + ${secondNumber}`
-    
+
     //DISPLAY ON -SCREEN-
-    document.getElementById("display").innerHTML = equation;
+    document.getElementById("display").innerHTML = `${firstNumber} + ${secondNumber}`;
     
     //DEBUG
-    console.log(`${firstNumber} + ${secondNumber}`);
+    console.log(`Prompt: ${firstNumber} + ${secondNumber}`);
 }
